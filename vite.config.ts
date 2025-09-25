63ef9f7daff621f4413468ac3fe79667332436f4
JayNightmare <jordanbell321@gmail.com>
2025-09-25 14:36:14 +0100

    Update homepage in package.json and remove dynamic repo base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 
-const repo = process.env.REPO_NAME || "phunparty";
-
 export default defineConfig({
     plugins: [react()],
-    base: `/${repo}/`,
     resolve: { alias: { "@": path.resolve(__dirname, "src") } },
     test: {
         environment: "jsdom",
